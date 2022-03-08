f97151863259f66214f67e9ee663e5e49414f3ff
moumted <461277664@qq.com>
2022-03-08 23:18:55 +0800

    home

--- main.js
+++ main.js
@@ -2,11 +2,31 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import { $http } from '@escook/request-miniprogram'
 
 Vue.config.productionTip = false
 
+
 App.mpType = 'app'
 
+uni.$http = $http
+$http.baseUrl = "https://www.uinav.com"
+$http.beforeRequest = function(options){
+  uni.showLoading({
+    title : '数据加载中....'
+  })
+}
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+uni.$showMsg = function(title=' 数据请求失败',duration=1500){
+  uni.showToast({
+    title,
+    duration,
+    icon : 'none'
+  })
+}
 const app = new Vue({
     ...App
 })
